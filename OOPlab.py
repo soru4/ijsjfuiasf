051cbee2063a6f5e09ec182977eded1becec9a4f
soru4 <73024932+soru4@users.noreply.github.com>
2022-04-27 11:18:18 -0500

    Update OOPlab.py

--- OOPlab.py
+++ OOPlab.py
@@ -2,3 +2,19 @@
 # we create a emploee class and we do a lot of inheritance
 
 
+class Company():
+  def __init__(self, employee_name, id):
+    self.employee_name = employee_name
+    self.id = id
+  def display(self):
+    return ("Name: " + self.employee_name+ " ID: " + str(self.id))
+emp1 = Employee("John Smith", 124451421)
+print(emp1.display())
+class Engineer(Company):
+  def __init__(self, emp, salary):
+    super().__init__(emp.employee_name, emp.id)
+    self.salary = salary
+  def display(self):
+    
+    print(super().display() + "Salary: " + str(self.salary))
+Engineer_Department = 
